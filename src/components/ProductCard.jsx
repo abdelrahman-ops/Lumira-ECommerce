1074a61311485ec593bd52c214237b3b98fa853c
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-09-06 05:05:30 +0300

    update product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,27 +5,42 @@ import { Link } from "react-router-dom";
 
 const ProductCard = ({ _id, name, price, image }) => {
     const { currency } = useContext(shop);
-    console.log();
     
+    // const [isFavorited, setIsFavorited] = useState(false);
+
+    // const handleFavoriteClick = () => {
+    //     setIsFavorited(!isFavorited);
+    // };
+
     return (
-        <div className="text-gray-700 cursor-pointer">
-            <div className="overflow-hidden">
-                <img src={image} alt={name} className="hover:scale-110 transition ease-in-out" />
+        <Link to={`/product/${_id}`}>
+            <div className="text-gray-700 cursor-pointer">
+                <div className="overflow-hidden">
+                    <img src={image} alt={name} className="hover:scale-110 transition ease-in-out" />
+                </div>
+                <div>
+                    <p className="pt-3 pb-1 text-sm h-16">{name}</p>
+                    <p className="flex justify-between text-sm font-medium">
+                        {currency}{price}
+                        <div className="flex items-center gap-2">
+                            {/* <button className="p-0 m-0" onClick={handleFavoriteClick}>
+                                <span className={`material-symbols-outlined transition ease-in-out ${
+                                isFavorited ? 'text-red-600' : 'text-gray-500'
+                            }`}>favorite</span> 
+                            </button> */}
+                            <button className="p-0 m-0">
+                                <span className="material-symbols-outlined">favorite</span> 
+                            </button>
+                            <button className="p-0 m-0">
+                                <span className="material-symbols-outlined">add_shopping_cart</span>
+                            </button>
+                        </div>
+                    </p>
+                </div>
             </div>
-            <div>
-                <p className="pt-3 pb-1 text-sm">{name}</p>
-                <p className="text-sm font-medium">
-                    {currency}
-                    {price}
-                </p>
-            </div>
-            <div>
-                <Link to={`/product/${_id}`} className="btn btn-primary">Watch More</Link>
-                <button className="btn btn-secondary">
-                    <span className="material-symbols-outlined">add_shopping_cart</span>
-                </button>
-            </div>
-        </div>
+        </Link>
+
+        
     );
 };
 
