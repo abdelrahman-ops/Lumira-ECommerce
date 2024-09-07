0051159f966c963d97e34b9d79c1ec4d7535dbb2
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-09-07 19:00:43 +0300

    handle auth error

--- src/customHook/AuthContext.jsx
+++ src/customHook/AuthContext.jsx
@@ -7,36 +7,27 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [user,setUser] = useState(null);
-
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     // Check for token in cookies or localStorage on mount
     useEffect(() => {
         const token = Cookies.get("token");
-        const storedUser = Cookies.get("user");
-        if (token && setUser) {
+        if (token) {
             setIsAuthenticated(true);
-            setUser(JSON.parse(storedUser));
         }
     }, []);
 
-    const login = (userData) => {
-        Cookies.set("token", userData.token); // Store token in cookies
-        Cookies.set("user", JSON.stringify(userData)); // Store user data in cookies
-        setUser(userData); // Set user data in state
+    const login = () => {
         setIsAuthenticated(true);
     };
 
     const logout = () => {
         Cookies.remove("token");
-        Cookies.remove("user"); // Remove user data from cookies
-        setUser(null); // Clear user data in state
         setIsAuthenticated(false);
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, user , login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
