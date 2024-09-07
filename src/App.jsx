105713015b2b89d03adf17c815198da5cc9be4db
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-09-07 18:24:36 +0300

    add cart and profile pages routes and update login , provide protected routes , add toast conatiner also provide authentication using context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react-refresh/only-export-components */
 // import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createContext } from 'react';
+import { ToastContainer } from 'react-toastify';
+
+import { AuthProvider } from './customHook/AuthContext';
+import  PrivateRoute  from './customHook/PrivateRoute';
 
 import Home from './pages/Home';
 import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 import Nav3 from './components/Nav3';
 import ProductDetail from './components/ProductDetail';
@@ -15,10 +22,12 @@ import './css/App.css';
 import './css/Nav.css';
 import './css/Hero.css';
 import './css/Settler.css';
-import { createContext } from 'react';
+
 
 import { products } from "./assets/frontend_assets/assets";
-import Login from './pages/Login';
+import Profile from './pages/Profile';
+
+
 
 export const shop = createContext();
 
@@ -32,22 +41,32 @@ function App() {
 	return (
 		<>
 		<shop.Provider value={value}>
-			<Router>
-				<div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-					<Nav3 />
-					
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/collection' element={<Collection />} />
-						<Route path='/about' element={<About />} />
-						<Route path='/contact' element={<Contact />} />
-						<Route path='/product/:id' element={<ProductDetail />} />
-						<Route path='/login' element={<Login />} />
-					</Routes>
-					
-					<Footer />
-				</div>
-			</Router>
+			<AuthProvider>
+				<Router>
+					<div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+						<Nav3 />
+						<ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/collection' element={<Collection />} />
+							<Route path='/about' element={<About />} />
+							<Route path='/contact' element={<Contact />} />
+							<Route path='/login' element={<Login />} />
+							<Route path='/profile' element={<Profile />} />
+							<Route 
+								path='/cart' 
+								element={
+									<PrivateRoute>
+										<Cart />
+									</PrivateRoute>
+									} />
+							<Route path='/product/:id' element={<ProductDetail />} />
+						</Routes>
+						
+						<Footer />
+					</div>
+				</Router>
+			</AuthProvider>
 		</shop.Provider>
 		</>
     
