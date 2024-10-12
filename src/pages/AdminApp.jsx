74c22f184a1294220df85e118d77a6f7f6473b71
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-13 01:10:39 +0300

    add admin app

---
+++ src/pages/AdminApp.jsx
@@ -0,0 +1,19 @@
+// AdminApp.jsx
+import SideBar from '../components/SideBar';
+import AdminNav from '../components/AdminNav';
+
+
+const AdminApp = () => {
+    return (
+        <>
+            
+                <AdminNav />
+                <hr />
+                <SideBar />
+            
+            
+        </>
+    );
+};
+
+export default AdminApp;
