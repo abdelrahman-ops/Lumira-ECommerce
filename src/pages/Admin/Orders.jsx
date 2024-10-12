81374e7b1cc501dd367d2cf634e8383a34248b33
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-13 01:12:27 +0300

    Fetching Orders -not completed

---
+++ src/pages/Admin/Orders.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const Orders = () => {
+    return (
+        <>
+        <div>
+            <h3>Order Page</h3>
+            <div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Orders
