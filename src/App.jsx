b0798ef75cd838937c0f2f95c820cb9d4bd4135d
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-27 04:30:08 +0300

    update routes and auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
-import { BrowserRouter as Router, Route, Routes , useMatch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { createContext } from 'react';
 import { ToastContainer } from 'react-toastify';
 
-import { AuthProvider } from './customHook/AuthContext';
-import {DataProvider} from './customHook/DataContext';
-import { CartProvider } from './customHook/CartContext';
-import { ProductProvider } from './customHook/ProductContext';
+import { AuthProvider } from './hooks/AuthContext';
+import {DataProvider} from './hooks/DataContext';
+import { CartProvider } from './hooks/CartContext';
+import { ProductProvider } from './hooks/ProductContext';
 
 import Home from './pages/Home';
-// import Home2 from './pages/Home2';
 import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -21,14 +20,13 @@ import WishList from './pages/WishList';
 import Register from './pages/Register';
 
 
-import Nav3 from './components/Nav3';
+import Navbar from './components/Navbar';
 import ProductDetail from './components/ProductDetail';
 import Footer from './components/Footer';
 
 import { products } from "./assets/frontend_assets/assets";
 
 import ProtectedRoute from './components/ProtectedRoute';
-import AdminRoutes from './components/AdminRoutes';
 
 
 
@@ -65,11 +63,9 @@ function App() {
 
 
 const Main = () => {
-	const isAdminPath = useMatch('/admin/*');
-
 	return (
-		<div className={isAdminPath ? 'bg-gray-50 min-h-screen' : 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'}>
-			{!isAdminPath && <Nav3 />}
+		<div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+			<Navbar />
 			<ToastContainer position="top-right" autoClose={3000} hideProgressBar />
 			<Routes>
 				<Route path='/' element={<Home />} />
@@ -78,16 +74,16 @@ const Main = () => {
 				<Route path='/contact' element={<Contact />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
-				<Route path='/profile' element={<Profile />} />
+				<Route path='/profile' element={<ProtectedRoute> <Profile /> </ProtectedRoute>} />
 				<Route path='/error' element={<Error />} />
-				<Route path='/cart' element={<ProtectedRoute> <Cart /> </ProtectedRoute>} />
+				<Route path='/cart' element={ <Cart /> } />
 				<Route path='/wishlist' element={<WishList />} />
 				<Route path='/product/:id' element={<ProductDetail />} />
-				<Route path='/admin/*' element={<AdminRoutes />} />
-			</Routes>
-			{!isAdminPath && <Footer />}
+            </Routes>
+			<Footer />
 		</div>
 	);
 }
+
 
 export default App;
