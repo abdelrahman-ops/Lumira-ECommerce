d33048a5bf49bde3c2648c18cb58d2605e6c089f
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-27 04:31:17 +0300

    update imports

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../customHook/AuthContext";
+import { useAuth } from "../hooks/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
 
